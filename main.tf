8087eed5b237182f48120fd0fb9f4ec2c1f1192d
Filipe Siqueira <filipedev040990@gmail.com>
2024-03-12 20:23:01 -0300

    chore: change permission for lambda function

--- main.tf
+++ main.tf
@@ -4,10 +4,9 @@ resource "null_resource" "create_lambda_zip" {
   }
 
   provisioner "local-exec" {
-    command = "zip -r lambda.zip src/*"
+    command = "cd dist && zip -r ../lambda.zip index.js response.js && cd .. && zip -r lambda.zip node_modules"
   }
 }
-
 
 provider "aws" {
   region = "us-east-1"
@@ -21,31 +20,34 @@ resource "aws_lambda_function" "auth_lambda" {
   handler          = "index.handler" 
   runtime          = "nodejs20.x"
   timeout          = 10 
+
+  environment {
+    variables = {
+      TABLE_USERS = "users"
+    }
+  }
+  depends_on = [null_resource.create_lambda_zip]
 }
 
 # Definindo a política de execução para a função Lambda
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda-exec-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 # Anexando a política de execução à função Lambda
 resource "aws_iam_policy_attachment" "lambda_execution" {
   name       = "lambda-execution-policy"
   roles      = [aws_iam_role.lambda_exec.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBReadOnlyAccess"
 }
